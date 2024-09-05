d688f815c073a69cf61c52568040c54750d9f14e
Derik Gomide Padua <tiger.dgp@hotmail.com>
2024-09-05 12:14:52 -0300

    Atualização do código com base na aula 04

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-console.log(dados[1]);
+function pesquisar() {
+  // Seleciona o elemento HTML com o ID "novel" onde os resultados serão exibidos.
+  let novel = document.getElementById('novel');
 
-let novel = document.getElementById('novel');
+  // Inicializa uma string vazia para armazenar o HTML dos resultados.
+  let result = "";
 
-for (let i = 0; i < dados.length; i++) {
-	novel.innerHTML += `<div class="item-resultado">
-			<div class="bloco-esq">
-				<h2>${dados[i].titulo}</h2>
-				<div class="descricao-meta">
-					${dados[i].descricao}
-				</div>
-				<ul>
-					<li><span>Autor:</span> ${dados[i].autor}</li>
-					<li><span>Capítulos:</span> ${dados[i].capitulo}</li>
-					<li><span>Status:</span> ${dados[i].status}</li>
-				</ul>
-				<div class="item-mais">
-					<a href="${dados[i].link}" target="_blank">Mais Informações</a>
-				</div>
-			</div>
-			<div class="bloco-dir">
-				<img src="${dados[i].img}">
-			</div>
-		</div>`;
+  // Itera sobre cada novela no array de dados.
+  for (let dado of dados) {
+    // Cria uma div para cada novela, com as informações relevantes.
+    result += `<div class="item-resultado">
+                <div class="bloco-esq">
+                  <h2>${dado.titulo}</h2> <div class="descricao-meta">
+                    ${dado.descricao} </div>
+                  <ul>
+                    <li><span>Autor:</span> ${dado.autor}</li> <li><span>Capítulos:</span> ${dado.capitulo}</li> <li><span>Status:</span> ${dado.status}</li> </ul>
+                  <div class="item-mais">
+                    <a href="${dado.link}" target="_blank">Mais Informações</a> </div>
+                </div>
+                <div class="bloco-dir">
+                  <img src="${dado.img}"> </div>
+              </div>`;
+  }
+
+  // Atribui o HTML gerado ao elemento "novel", substituindo o conteúdo anterior.
+  novel.innerHTML = result;
 }
